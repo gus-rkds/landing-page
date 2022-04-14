ec2d090338b547c8101542c6da5f63cc34b5635a
Gustavo Alviarez <gusalv2001@gmail.com>
2022-04-14 09:29:24 -0400

    fixing cdn problem

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,6 @@ export default function Home() {
       <Head>
         <title>FOODO - Landign Page</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-          crossOrigin="anonymous"
-        ></link>
       </Head>
 
       <TopContainer />
@@ -33,20 +21,6 @@ export default function Home() {
 
       <footer className="footer">
         <h5>Foodo &copy; gus_rkds</h5>
-        <script
-          src="https://unpkg.com/react/umd/react.production.min.js"
-          crossOrigin
-        ></script>
-
-        <script
-          src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-          crossOrigin
-        ></script>
-
-        <script
-          src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-          crossOrigin
-        ></script>
       </footer>
     </div>
   );
